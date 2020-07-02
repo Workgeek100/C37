5bd22f3146218affee0ef1a5a75bd7211ed469db
Workgeek100 <maybha_70@hotmail.com>
2020-07-02 19:21:45 +0300

    C37

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var canvas, bg, database, form, player, game, playerCount,allPlayers;
+var gameState=0;
+var cars,car1,car2,car3,car4;
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(displayWidth-20,displayHeight-30);
+  game = new Game();
+  game.getState();
+  game.start();
+  
+
+
+  
+}
+
+function draw(){
+  if(playerCount===4){
+    game.update(1);
+
+  }
+  if(gameState===1){
+    clear();
+    game.play();
+  }
+    
+}
+
+
